e0fdc1ccbcc2874ad13847709efc7ab55e70ce17
hiten <hitengupta12345@gmail.com>
2025-08-08 21:36:53 +0930

    h

--- main.cpp
+++ main.cpp
@@ -15,7 +15,7 @@ int main() {
     cout << c3.freeParks("SSD-B---BD-DDSB-----S-S--------S-B----BSB-S--B-S-D") << endl;
 
     CircularLine c4;
-    cout << c4.longestTravel({1,1,1,1,4}) << endl;
+    cout << c4.longestTravel({1,4,4,1,5}) << endl;
 
     return 0;
 }
--- week1/CircularLine/CircularLine.hpp
+++ week1/CircularLine/CircularLine.hpp
@@ -4,33 +4,33 @@ using namespace std;
 class CircularLine {
   public:
     int n, ans;
+    vector<int> prefix;
 
     int longestTravel(vector<int> t) {
         n = t.size(), ans = 0;
+        createPrefixSuffix(t);
         for (int i = 0; i < n; i++) {
             for (int j = i + 1; j < n; j++) {
-                ans = max(calcDis(i, j, t), ans);
+                ans = max(calcDis(i, j), ans);
             }
         }
 
         return ans;
-    }
-
-    int calcDis(int x, int y, vector<int> &d) {
-        // forward dis
-        int forDis = dis(x, y, d);
-        // backDis
-        int backDis = dis(0, x, d) + dis(y, n, d);
-
-        return min(backDis, forDis);
     };
 
-    int dis(int x, int y, vector<int> &vec) {
-        int dis = 0;
-        for (int i = x; i < y; i++) {
-            dis += vec[i];
-        }
+    void createPrefixSuffix(vector<int> dis) {
+        prefix = vector<int>(n + 1, 0);
 
-        return dis;
+        // populating prefix
+        for (int i = 1; i <= n; i++) {
+            prefix[i] = prefix[i - 1] + dis[i - 1];
+        }
+    };
+
+    int calcDis(int x, int y) {
+        int forDis = prefix[y] - prefix[x]; // forward dis
+        int backDis = prefix[x] - prefix[0] + prefix[n] - prefix[y]; // backDis
+
+        return min(backDis, forDis);
     };
 };
