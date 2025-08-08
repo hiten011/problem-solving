a969f22758f1f5c717dfedac66bc5a0ffa862c59
hiten <hitengupta12345@gmail.com>
2025-08-08 19:29:41 +0930

    h

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 // personal files
 #include "week1/Inchworm/Inchworm.hpp"
+#include "week1/ThrowTheBall/ThrowTheBall.hpp"
 
 int main() {
     Inchworm c1;
     cout << c1.lunchtime(11, 2, 4) << endl;
+
+    ThrowTheBall c2;
+    cout << c2.timesThrown(5, 3, 2) << endl;
     return 0;
 }
