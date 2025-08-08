f5e35e449a3d8d2a92e1c95f8bf727cbf8cf4030
hiten <hitengupta12345@gmail.com>
2025-08-08 19:10:44 +0930

    h

---
+++ Inchworm.hpp
@@ -0,0 +1,9 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class Inchworm {
+  public:
+    int lunchtime(int branch, int rest, int leaf) {
+        return (int) (branch / lcm(rest, leaf)) + 1;
+    }
+};
---
+++ main.cpp
@@ -0,0 +1,11 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+// personal files
+#include "Inchworm.hpp"
+
+int main() {
+    Inchworm c1;
+    cout << c1.lunchtime(11, 2, 4) << endl;
+    return 0;
+}
