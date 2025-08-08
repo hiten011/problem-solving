c29d80ce77bf0c5d52085d8844c4e1b61edd5e25
hiten <hitengupta12345@gmail.com>
2025-08-08 22:20:16 +0930

    h

--- week1/PartySeats/PartySeats.hpp
+++ week1/PartySeats/PartySeats.hpp
@@ -35,6 +35,8 @@ class PartySeats {
             if ((n / 2) + 1 == i) ans[i] = "HOSTESS";
             else if (i % 2 == 1) ans[i] = girls[ptr1++];
             else ans[i] = boys[ptr2++];
+
+            // cout << ans[i] << " ";
         }
 
         return ans;
