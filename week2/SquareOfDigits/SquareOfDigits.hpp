43eabc25aa00b75aba7baf36bfaebc6769c3eda9
hiten <hitengupta12345@gmail.com>
2025-08-11 22:59:59 +0930

    h

--- week2/SquareOfDigits/SquareOfDigits.hpp
+++ week2/SquareOfDigits/SquareOfDigits.hpp
@@ -3,23 +3,19 @@ using namespace std;
 
 class SquareOfDigits { 
     public:
-        vector<vector<int>> dir = {{1, 0}, {1, 1}, {0, 1}};
         int getMax(vector<string> data) {
             int ans = 1;
-            int n = data[0].size(), m = data.size();
-            vector<vector<int>> dp(m, vector<int>(n, 1));
+            int m= data[0].size(), n = data.size();
 
-            for (int i = m - 2; i >= 0; i--) {
-                for (int j = n - 2; j >= 0; j--) {
-                    int maxSquare = INT_MAX;
-                    
-                    for (auto &d : dir) {
-                        if (data[i][j] == data[i + d[0]][j + d[1]]) maxSquare = min(dp[i + d[0]][j + d[1]], maxSquare);
-                        else maxSquare = 0;
+            for (int i = 0; i < n; i++) {
+                for (int j = 0; j < m; j++) {
+                    int idx = 0;
+                    while (i + idx < n && j + idx < m) {
+                        char num = data[i][j];
+                        if (data[i + idx][j] == num && data[i][j + idx] == num && data[i + idx][j + idx] == num) ans = max(idx + 1, ans);
+
+                        idx++;
                     }
-
-                    dp[i][j] = maxSquare + 1;
-                    ans = max(dp[i][j], ans);
                 }
             }
 
