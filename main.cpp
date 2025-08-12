0f0b59545520c61c4915c80bd87084b0d7fbfae9
hiten <hitengupta12345@gmail.com>
2025-08-12 18:16:53 +0930

    h

--- main.cpp
+++ main.cpp
@@ -2,6 +2,11 @@
 #include "crazyRobot.hpp"
 
 int main() {
-    CrazyRobot c3;
+    CrazyRobot c2;
+    printf("%0.20f\n", c2.getProb(1, 0.25, 0.25, 0.25, 0.25));
+    printf("%0.20f\n", c2.getProb(2, 0.25, 0.25, 0.25, 0.25));
+    printf("%0.20f\n", c2.getProb(7, 0.5, 0, 0, 0.5));
+    printf("%0.20f\n", c2.getProb(14, 0.5, 0.5, 0, 0));
+    printf("%0.20f\n", c2.getProb(14, 0.25, 0.25, 0.25, 0.25));
     return 0;
 }
