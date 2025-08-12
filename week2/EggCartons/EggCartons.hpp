f8730ef4afe8aa7f24ef5ac257e2b08cbc46086d
hiten <hitengupta12345@gmail.com>
2025-08-12 19:43:27 +0930

    h

--- week2/EggCartons/EggCartons.hpp
+++ week2/EggCartons/EggCartons.hpp
@@ -7,11 +7,11 @@ public:
     int minCartons(int n) {
         if (n % 2 == 1) return -1;
         int x = 0;
-        while ((n - 6*x) >= 0 && (n - 6*x) % 8 != 0) {
+        while (n >= 6*x && (n - 6*x) % 8 != 0) {
             x++;
         }
 
-        return (n - 6 * x) < 0 ? -1 : x + ((n - 6 * x) / 8);
+        return n < 6*x ? -1 : x + ((n - 6 * x) / 8);
 
         return 0;
     }
