fdcc8e6833a12b494ff6005b6ee67afa6964b892
hiten <hitengupta12345@gmail.com>
2025-08-12 19:34:52 +0930

    h

--- main.cpp
+++ main.cpp
@@ -18,10 +18,6 @@ int main() {
          << endl;
 
     EggCartons c2;
-    printf("%0.20f\n", c2.getProb(1, 0.25, 0.25, 0.25, 0.25));
-    printf("%0.20f\n", c2.getProb(2, 0.25, 0.25, 0.25, 0.25));
-    printf("%0.20f\n", c2.getProb(7, 0.5, 0, 0, 0.5));
-    printf("%0.20f\n", c2.getProb(14, 0.5, 0.5, 0, 0));
-    printf("%0.20f\n", c2.getProb(14, 0.25, 0.25, 0.25, 0.25));
+    cout << c2.minCartons(15) << endl;
     return 0;
 }
