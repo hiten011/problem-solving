7a7b771346c50202d0eb3a539fd2ded74d42acbb
hiten <hitengupta12345@gmail.com>
2025-08-15 23:57:36 +0930

    h

--- week2/ChristmasTree/ChristmasTree.hpp
+++ week2/ChristmasTree/ChristmasTree.hpp
@@ -6,58 +6,128 @@ class ChristmasTree
 public:
     long long decorationWays(int N, int red, int green, int blue)
     {
-        vector<int> vec(1, -1); // [R: 0, B: 1, G: 2]
-        return helper(1, N, red, green, blue, 0, vec);
+        return helper(1, N, red, green, blue);
     }
 
-    long long helper(int lvl, int maxLvl, int red, int green, int blue, int idx, vector<int> &vec) {
+private:
+    long long helper(int lvl, int maxLvl, int red, int green, int blue) {
         if (lvl > maxLvl) return 1;
-        if (idx == lvl) {
-            if (isValid(vec)) {
-                vector<int> temp(lvl + 1, -1);
-                return helper(lvl + 1, maxLvl, red, green, blue, 0, temp);
-            } else {
-                return 0;
-            }
-        }
         
         long long ans = 0;
 
-        // try to place red
-        if (red > 0) {
-            vec[idx] = 0;
-            ans += helper(lvl, maxLvl, red - 1, green, blue, idx + 1, vec);
+        vector<int> vec(3, 0); // [red: 0, green: 1, blue: 2]
+
+        // try all red
+        if (red - lvl >= 0) {
+            vec[0] = lvl;
+
+            ans += helper(lvl + 1, maxLvl, red - lvl, green, blue) * perm(vec);
+
+            vec[0] = 0;
         }
 
-        // try to plac blue
-        if (blue > 0) {
-            vec[idx] = 1;
-            ans += helper(lvl, maxLvl, red, green, blue - 1, idx + 1, vec);
+        // try all green
+        if (green - lvl >= 0) {
+            vec[1] = lvl;
+
+            ans += helper(lvl + 1, maxLvl, red, green - lvl, blue) * perm(vec);
+
+            vec[1] = 0;
         }
 
-        // try to place green
-        if (green > 0) {
-            vec[idx] = 2;
-            ans += helper(lvl, maxLvl, red, green - 1, blue, idx + 1, vec);
+        // try all blue
+        if (blue - lvl >= 0) {
+            vec[2] = lvl;
+
+            ans += helper(lvl + 1, maxLvl, red, green, blue - lvl) * perm(vec);
+
+            vec[2] = 0;
         }
 
-        vec[idx] = -1;
+        // try to divide bwtween all three
+        if (lvl % 3 == 0) {
+            int num = lvl / 3;
+            if (red < num || blue < num || green < num) {} 
+            else {
+                // setting to num
+                for (int i = 0; i < 3; i++) {
+                    vec[i] = num;
+                }
+
+                ans += helper(lvl + 1, maxLvl, red - num, green - num, blue - num)  * perm(vec);
+                
+                // reset
+                for (int i = 0; i < 3; i++) {
+                    vec[i] = 0;
+                }
+            }
+        }
+
+        // try red-blue and red-green
+        if (lvl % 2 == 0) {
+            int num = lvl / 2;
+
+            if (red >= num) {
+                vec[0] = num;
+
+                // try blue
+                if (blue >= num) {
+                    vec[2] = num;
+
+                    ans += helper(lvl + 1, maxLvl, red - num, green, blue - num)  * perm(vec);
+
+                    vec[2] = 0;
+                }
+
+                // try green
+                if (green >= num) {
+                    vec[1] = num;
+
+                    ans += helper(lvl + 1, maxLvl, red - num, green - num, blue) * perm(vec);
+
+                    vec[1] = 0;
+                }
+
+                vec[0] = 0;
+            }
+        }
+
+        // try blue-green
+        if (lvl % 2 == 0) {
+            int num = lvl / 2;
+
+            if (blue >= num && green >= num) {
+                vec[1] = num;
+                vec[2] = num;
+
+                ans += helper(lvl + 1, maxLvl, red, green - num, blue - num)  * perm(vec);
+
+                vec[1] = 0;
+                vec[2] = 0;
+            }
+        }
+
         return ans;
     }
 
-    bool isValid(vector<int> &vec) {
-        vector<int> freq(3, 0);
-        int record = 0;
-        for (int i : vec) {
-            freq[i]++;
-            record = freq[i];
+    long long perm(vector<int> &freq) {
+        long long ans = 0;
+        ans += fact(freq[0] + freq[1] + freq[2]);
+        
+        for (int i : freq) {
+            ans /= fact(i);
         }
 
-        if (freq[0] == 0) freq[0] = record;
-        if (freq[1] == 0) freq[1] = record;
-        if (freq[2] == 0) freq[2] = record;
+        return ans;
+    };
 
-        if (freq[0] == freq[1] && freq[1] == freq[2]) return true;
-        return false;
+    unordered_map<int, long long> factorials;
+    long long fact(int num) {
+        if (num == 0) return 1;
+        if (factorials.count(num) != 0) {
+            return factorials[num];
+        }
+
+        return factorials[num] = num * fact(num - 1);
     }
 };
