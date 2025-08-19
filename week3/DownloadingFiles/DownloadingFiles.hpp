ce6018676f051480a8bb3ac9df43f43034d05d68
hiten <hitengupta12345@gmail.com>
2025-08-19 15:25:42 +0930

    h

---
+++ week3/DownloadingFiles/DownloadingFiles.hpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class DownloadingFiles {
+public:
+    double actualTime(vector<string> tasks) {
+        vector<pair<int, int>> vec;
+        for (string str : tasks) {
+            continue;
+        }
+
+        return 0.0;
+    }
+private:
+};
