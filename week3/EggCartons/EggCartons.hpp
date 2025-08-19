a1d390492654ff06ecafc4d1eeb53cf1daea04a8
hiten <hitengupta12345@gmail.com>
2025-08-19 15:49:36 +0930

    h

---
+++ week3/EggCartons/EggCartons.hpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class EggCartons
+{
+public:
+    int minCartons(int n)
+    {
+        if (n % 2 == 1)
+            return -1;
+
+        int x = 0;
+        while (n >= 6 * x && x <= 3 && (n - 6 * x) % 8 != 0)
+        {
+            x++;
+        }
+
+        return n < 6 * x ? -1 : x + ((n - 6 * x) / 8);
+    }
+};
