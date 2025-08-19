617d6e3fabdca1ad1c3760b8455ae4efccaef610
hiten <hitengupta12345@gmail.com>
2025-08-19 15:32:58 +0930

    h

--- week2/EggCartons/EggCartons.hpp
+++ week2/EggCartons/EggCartons.hpp
@@ -6,13 +6,12 @@ class EggCartons
 public:
     int minCartons(int n) {
         if (n % 2 == 1) return -1;
+        
         int x = 0;
         while (n >= 6*x && (n - 6*x) % 8 != 0) {
             x++;
         }
 
         return n < 6*x ? -1 : x + ((n - 6 * x) / 8);
-
-        return 0;
     }
 };
