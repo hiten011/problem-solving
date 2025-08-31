fe4bbcce9b20beabebff148659556d5a1e5fc808
hiten <hitengupta12345@gmail.com>
2025-08-31 17:05:00 +0930

    d

--- main.cpp
+++ main.cpp
@@ -1,5 +1,8 @@
 // personal files
+#include "week4/MonstersValley2.hpp"
 
 int main() {
+    MonstersValley2 c1;
+    cout << c1.minimumPrice({8, 5, 10}, {1, 1, 2}) << endl;
     return 0;
 }
---
+++ week4/MonstersValley2.hpp
@@ -0,0 +1,18 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class MonstersValley2 {
+    public:
+        int minimumPrice(vector<int> dread, vector<int> price, int idx = 0, int party = 0, int cost = 0) {
+            if (idx == dread.size()) return cost;
+            
+            // bribe
+            int ans = minimumPrice(dread, price, idx + 1, party + dread[idx], cost + price[idx]);
+            if (party >= dread[idx]) {
+                // not-bribe
+                ans = min(minimumPrice(dread, price, idx + 1, party, cost), ans);
+            }
+
+            return ans;
+        }
+};
