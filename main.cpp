c9cbbc27bc663f61fd7496443e16b5951b353427
hiten <hitengupta12345@gmail.com>
2025-08-31 21:39:24 +0930

    d

--- main.cpp
+++ main.cpp
@@ -1,8 +1,12 @@
 // personal files
 #include "week4/MonstersValley2/MonstersValley2.hpp"
+#include "week4/UnsealTheSafe/UnsealTheSafe.hpp"
 
 int main() {
     MonstersValley2 c1;
     cout << c1.minimumPrice({8, 5, 10}, {1, 1, 2}) << endl;
+
+    UnsealTheSafe c2;
+    cout << c2.countPasswords(3) << endl;
     return 0;
 }
---
+++ week4/UnsealTheSafe/UnsealTheSafe.hpp
@@ -0,0 +1,31 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class UnsealTheSafe
+{
+public:
+    long countPasswords(int N, int num = 10) {
+        if (N == 0) return 1;
+
+        long ans = 0;
+        for (int i : adj[num]) {
+            ans += countPasswords(N - 1, i);
+        }
+
+        return ans;
+    }
+
+private:
+    vector<vector<int>> adj = {
+        /* 0 */ {7},
+        /* 1 */ {2, 4},
+        /* 2 */ {1, 3, 5},
+        /* 3 */ {2, 6},
+        /* 4 */ {1, 5, 7},
+        /* 5 */ {2, 4, 6, 8},
+        /* 6 */ {3, 5, 9},
+        /* 7 */ {4, 8, 0},
+        /* 8 */ {5, 7, 9},
+        /* 9 */ {6, 8},
+        /* 10 */ {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}};
+};
