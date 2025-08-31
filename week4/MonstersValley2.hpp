3e474de4adaf4eafb323202ef980689ec8c21c5f
hiten <hitengupta12345@gmail.com>
2025-08-31 17:09:01 +0930

    d

--- week4/MonstersValley2.hpp
+++ week4/MonstersValley2.hpp
@@ -3,14 +3,19 @@ using namespace std;
 
 class MonstersValley2 {
     public:
-        int minimumPrice(vector<int> dread, vector<int> price, int idx = 0, int party = 0, int cost = 0) {
+        int minimumPrice(vector<int> dread, vector<int> price)
+        {
+            return helper(dread, price, 0, 0, 0);
+        }
+
+        int helper(vector<int> &dread, vector<int> &price, int idx = 0, int party = 0, int cost = 0) {
             if (idx == dread.size()) return cost;
             
             // bribe
-            int ans = minimumPrice(dread, price, idx + 1, party + dread[idx], cost + price[idx]);
+            int ans = helper(dread, price, idx + 1, party + dread[idx], cost + price[idx]);
             if (party >= dread[idx]) {
                 // not-bribe
-                ans = min(minimumPrice(dread, price, idx + 1, party, cost), ans);
+                ans = min(helper(dread, price, idx + 1, party, cost), ans);
             }
 
             return ans;
