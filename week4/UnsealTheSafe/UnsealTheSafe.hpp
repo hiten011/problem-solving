abaa379d3a38cf64e826d9d274ce3b35a8fdc2ba
hiten <hitengupta12345@gmail.com>
2025-08-31 21:55:36 +0930

    d

--- week4/UnsealTheSafe/UnsealTheSafe.hpp
+++ week4/UnsealTheSafe/UnsealTheSafe.hpp
@@ -3,21 +3,32 @@ using namespace std;
 
 class UnsealTheSafe {
 public:
-    long countPasswords(int N, int num = 10) {
-        if (N == 0) return 1;
-        
-        if (memo.count(N) > 0 && memo[N].count(num) > 0) return memo[N][num];
-        
-        long ans = 0;
-        for (int i : adj[num]) {
-            ans += countPasswords(N - 1, i);
+    long countPasswords(int N) {
+        vector<vector<long>> dp(N, vector<long>(10, 0));
+        for (int i = 0; i < 10; i++) {
+            dp[N - 1][i] = 1;
+        } 
+
+        for (int i = N - 2; i >= 0; i--) {
+            for (int j = 0; j < 10; j++) {
+                long cur = 0;
+                for (int k : adj[j]) {
+                    cur += dp[i + 1][k];
+                }
+
+                dp[i][j] = cur;
+            }
         }
 
-        return memo[N][num] = ans;
+        long ans = 0;
+        for (int i = 0; i < 10; i++) {
+            ans += dp[0][i];
+        }
+
+        return ans;
     }
 
 private:
-    unordered_map<int, unordered_map<int, long>> memo;
     vector<vector<int>> adj = {
         /* 0 */ {7},
         /* 1 */ {2, 4},
@@ -28,6 +39,5 @@ private:
         /* 6 */ {3, 5, 9},
         /* 7 */ {4, 8, 0},
         /* 8 */ {5, 7, 9},
-        /* 9 */ {6, 8},
-        /* 10 */ {1, 2, 3, 4, 5, 6, 7, 8, 9, 0}};
+        /* 9 */ {6, 8}};
 };
