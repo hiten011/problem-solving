4f9d048d5b2820745973796eacb639ac5e9b5fd0
hiten <hitengupta12345@gmail.com>
2025-09-12 19:34:39 +0930

    Week4

--- main.cpp
+++ main.cpp
@@ -5,8 +5,5 @@ int main() {
     JumpyNum c1;
     cout << c1.howMany(8000, 20934) << endl;
 
-    // TimeTravellingCellar c2;
-    // cout << c2.determineProfit({3, 2}, {1, 2})
-    //      << endl;
     return 0;
 }
--- week6/JumpyNum/JumpyNum.hpp
+++ week6/JumpyNum/JumpyNum.hpp
@@ -1,73 +1,40 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class JumpyNum
-{
-public:
-    int howMany(int low, int high)
-    {
-        if (low < 1)
-            low = 1;
-        return count(to_string(high)) - count(to_string(low - 1));
-    }
+class JumpyNum {
+    public:
+        int howMany(int low, int high) {
+            if (low == 0) low = 1;
+            string l = to_string(low - 1), h = to_string(high);
+            int ans = helper(h, 0, true, 11);
+            // cout << ans << endl;
 
-private:
-    int count(string num)
-    {
-        int n = num.size();
-        if (n == 1)
-            return getInt(num[0]) + 1;
+            int lAns = (l.size() == 1 ? getInt(l[0]) + 1 : helper(l, 0, true, 11));
+            // cout << lAns << endl;
 
-        vector<vector<int>> dp(n, vector<int>(11, 0));
-        for (int i = 0; i < 10; i++)
-        {
-            dp[n - 1][i] = 1;
+            return ans - lAns;
         }
+    
+    private:
+        int helper(string &num, int idx, bool isUpper, int prev) {
+            if (idx == num.size()) return 1;
+            
+            int ans = 0, en = (isUpper ? getInt(num[idx]) : 9), st = (prev == 11 ? 1 : 0);
+            for (int i = st; i <= en; i++) {
+                if (i == prev || i == prev + 1 || i == prev - 1) continue;
 
-        for (int i = n - 2; i >= 0; i--)
-        {
-            for (int j = 0; j < 10; j++)
-            {
-                for (int k = 0; k < 10; k++)
-                {
-                    if (k == j || k == j + 1 || k == j - 1)
-                        continue;
-                    dp[i][j] += dp[i + 1][k];
+                if (isUpper && i == en) {
+                    ans += helper(num, idx + 1, true, i);
+                } else {
+                    ans += helper(num, idx + 1, false, i);
                 }
             }
 
-            for (int j = 0; j < 11; j++)
-            {
-                dp[i][10] += dp[i + 1][j];
-            }
+            if (idx != num.size() - 1 && prev == 11) ans += helper(num, idx + 1, false, 11);
+            return ans;
+        }  
+
+        int getInt(char c) {
+            return (int) (c - '0');
         }
-
-        int ans = 1;
-        for (int i = n - 2; i >= 0; i--)
-        {
-            int h = getInt(num[i]), sum = 0;
-            for (int j = 0; j <= getInt(num[i + 1]); j++)
-            {
-                if (h == j + 1 || h == j || h == j - 1)
-                    continue;
-
-                if (j == getInt(num[i + 1]))
-                    sum += ans;
-                else
-                    sum += dp[i + 1][j];
-            }
-
-            ans = sum;
-        }
-
-        for (int i = 1; i < getInt(num[0]); i++)
-        {
-            ans += dp[0][i];
-        }
-        ans += dp[0][10];
-        cout << ans << endl;
-        return ans;
-    }
-
-    int getInt(char c) { return (int)(c - '0'); }
 };
