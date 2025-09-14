73096ea884a3dbfc7ee22985c1b702e4634c2e01
hiten <hitengupta12345@gmail.com>
2025-09-14 21:58:56 +0930

    Week4

--- main.cpp
+++ main.cpp
@@ -7,6 +7,6 @@ int main() {
     cout << c1.howMany(8000, 20934) << endl;
 
     NiceOrUgly c2;
-    cout << c2.describe("A???????A") << endl;
+    cout << c2.describe("?????") << endl;
     return 0;
 }
