404cbcce7dceab5775ed8a0066e46c319aedf955
hiten <hitengupta12345@gmail.com>
2025-09-15 01:25:30 +0930

    Week4

--- main.cpp
+++ main.cpp
@@ -1,5 +1,11 @@
 // personal files
+#include "week7/ShipBoxes/ShipBoxes.hpp"
 
 int main() {
+    ShipBoxes c1;
+    cout << c1.bestCost({1, 4, 9},
+
+                        {1, 4, 9})
+         << endl;
     return 0;
 }
