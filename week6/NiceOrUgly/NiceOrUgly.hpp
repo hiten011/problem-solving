5876faaf3aef4eff1f8f85675f6db6b73037bacc
hiten <hitengupta12345@gmail.com>
2025-09-14 22:09:55 +0930

    Week4

--- week6/NiceOrUgly/NiceOrUgly.hpp
+++ week6/NiceOrUgly/NiceOrUgly.hpp
@@ -5,85 +5,39 @@ class NiceOrUgly {
     public:
         string describe(string s) {
             int n = s.size();
+            int v = 0, c = 0;
+            bool isUgly = false, isNice = true;
+            for (int i = n - 1; i >= 0; i--) {
+                if (s[i] == '?') {
+                    v++;
+                    c++;
+                } else if (isVowel(s[i])) {
+                    v++;
+                    c = 0;
+                } else {
+                    c++;
+                    v = 0;
+                }
 
-            // cout << s << endl;
+                if (v >= 3) {
+                    isUgly = true;
+                }
 
-            string str = consc(s);
-            if (str != "-1") return str;
+                if (c >= 5) {
+                    isUgly = true;
+                }
 
-            bool isC = isUglyC(s, n);
-            bool isV = isUglyV(s, n);
-
-
-            size_t pos = s.find("?");
-            if (pos != string::npos) {
-                if (isC && isV) return "UGLY";
-                if (!isC && !isV) return "NICE";
-                return "42";
-            } else {
-                if (isC || isV) return "UGLY";
-                else return "NICE";
-            }
-        }
-
-    private:
-
-        string consc(string s) {
-            int count = 0;
-            for (int i = 0; i < s.size(); i++) {
-                if (s[i] == '?') count++;
-                else count = 0;
-
-                if (count == 5) {
-                    count--;
-                    while (i < s.size() && s[i] == '?') {
-                        count++;
-                        i++;
-                    }
-
-                    string rec1 = describe(s.substr(0, i - count));
-                    string rec2 = describe(s.substr(i));
-
-                    if (rec1 == "NICE" || rec1 == "42" || rec2 == "NICE" || rec2 == "42") return "42";
-                    else return "UGLY";
-                    break;
+                if (v >= 3 && c >= 5) {
+                    isNice = false;
                 }
             }
 
-            return "-1";
+            if (isUgly && isNice) return "42";
+            else if (isUgly) return "UGLY";
+            return "NICE";
         }
 
-        bool isUglyC(string s, int n) {
-            // check for Ugly
-            int en = 0, count = 0;
-            while (en < n) {
-                if (s[en] == '?') s[en] = 'C';
-
-                if (!isVowel(s[en])) count++;
-                else count = 0;
-
-                en++;
-                if (count == 5) return true;
-            }
-
-            return false;
-        }
-
-        bool isUglyV(string s, int n) {
-            // check for Ugly
-            int en = 0, count = 0;
-            while (en < n) {
-                if (s[en] == '?') s[en] = 'A';
-
-                if (isVowel(s[en])) count++;
-                else count = 0;
-
-                if (count == 3) return true;
-                en++;
-            }
-
-            return false;
-        }
+    private:
 
         bool isVowel(char c) {
             if (c == 'A' || c == 'E' || c == 'I' || c == 'O' || c == 'U') return true;
