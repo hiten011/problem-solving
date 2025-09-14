5aa7696c3b1eaa787da75c7beceee7ca2ec63fe4
hiten <hitengupta12345@gmail.com>
2025-09-14 20:40:14 +0930

    Week4

--- week6/JumpyNum/JumpyNum.hpp
+++ week6/JumpyNum/JumpyNum.hpp
@@ -1,39 +1,21 @@
 #include <bits/stdc++.h>
-
 using namespace std;
 
-
 class JumpyNum {
-
     public:
-
         int howMany(int low, int high) {
-
             if (low < 1) low = 1;
-
             return count(to_string(high)) - count(to_string(low - 1));
         }
-
-    
-
     private:
-
-
         int count(string num) {
-
             int n = num.size();
-
             if (n == 1) return getInt(num[0]) + 1;
 
-
             vector<vector<int>> dp(n, vector<int>(11, 0));
-
             for (int i = 0; i < 11; i++) {
-
                 dp[n - 1][i] = 1;
-
             }
-
 
             for (int i = n - 2; i >= 0; i--) {
 
@@ -44,16 +26,13 @@ class JumpyNum {
                         if (k == j || k == j + 1 || k == j - 1) continue;
 
                         dp[i][j] += dp[i + 1][k];
-
                     }
 
                 }
 
 
                 for (int j = 1; j < 11; j++) {
-
                     dp[i][10] += dp[i + 1][j];
-
                 }
 
             }
@@ -66,35 +45,23 @@ class JumpyNum {
                 int h = getInt(num[i]), sum = 0;
 
                 for (int j = 0; j <= getInt(num[i + 1]); j++) {
-
                     if (h == j + 1 || h == j || h == j - 1) continue;
 
-
                     if (j == getInt(num[i + 1])) sum += ans;
-
                     else sum += dp[i + 1][j];
-
                 }
 
-
                 ans = sum;
-
             }
 
-
             for (int i = 1; i < getInt(num[0]); i++) {
-
                 ans += dp[0][i];
-
             }
 
             ans += dp[0][10];
-
             return ans;
 
         }
 
-
         int getInt(char c) {return (int)(c - '0');}
-
 };
