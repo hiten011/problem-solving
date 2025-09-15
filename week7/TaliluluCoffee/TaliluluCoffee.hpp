76eab72be4d3f92fc73f7d5abf4c9e5480fe51ff
hiten <hitengupta12345@gmail.com>
2025-09-15 21:26:48 +0930

    Week4

---
+++ week7/TaliluluCoffee/TaliluluCoffee.hpp
@@ -0,0 +1,20 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class TaliluluCoffee
+{
+public:
+    	int maxTip(vector<int> tips) {
+            sort(tips.begin(), tips.end()); 
+
+            int ans = 0;
+            for (int i = 0; i < tips.size(); i++) {
+                if (tips[i] - i < 0) break;
+
+                ans += tips[i] - i;
+            }
+
+            return ans;
+        }
+private:
+};
