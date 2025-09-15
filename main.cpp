ef0c7de3c7bef24214f2b53ebbaeb81b834d1ee6
hiten <hitengupta12345@gmail.com>
2025-09-15 21:20:02 +0930

    Week4

--- main.cpp
+++ main.cpp
@@ -1,5 +1,6 @@
 // personal files
 #include "week7/ShipBoxes/ShipBoxes.hpp"
+#include "week7/PrefixFreeSets/PrefixFreeSets.hpp"
 
 int main() {
     ShipBoxes c1;
@@ -7,5 +8,8 @@ int main() {
 
                         {271, 818, 282})
          << endl;
+
+    PrefixFreeSets c2;
+    cout << c2.maxElements({"hello", "hi", "h", "run", "rerun", "running"}) << endl;
     return 0;
 }
--- week7/PrefixFreeSets/PrefixFreeSets.hpp
+++ week7/PrefixFreeSets/PrefixFreeSets.hpp
@@ -1,7 +1,7 @@
 #include <bits/stdc++.h>
 using namespace std;
 
-class ShipBoxes
+class PrefixFreeSets
 {
 public:
     int maxElements(vector<string> words) {
@@ -13,12 +13,16 @@ public:
             if (!isPrefix(prev, s)) {
                 ans++;
             }
+
+            prev = s;
         }
 
         return ans;
     }
 private:
     bool isPrefix(string &s1, string &s2) {
+        if(s1.empty()) return false;
+        
         for (int i = 0; i < min(s1.size(), s2.size()); i++) {
             if (s1[i] != s2[i]) return false;
         }
