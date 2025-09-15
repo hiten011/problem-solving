82369f8584700a7160974fe59d9c0d2d1fe6b3e8
hiten <hitengupta12345@gmail.com>
2025-09-15 21:29:45 +0930

    Week4

--- week7/TaliluluCoffee/TaliluluCoffee.hpp
+++ week7/TaliluluCoffee/TaliluluCoffee.hpp
@@ -5,7 +5,7 @@ class TaliluluCoffee
 {
 public:
     	int maxTip(vector<int> tips) {
-            sort(tips.begin(), tips.end()); 
+            sort(tips.begin(), tips.end(), greater<int>()); 
 
             int ans = 0;
             for (int i = 0; i < tips.size(); i++) {
