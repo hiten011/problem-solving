d709b62324eafb258e5562e1f0871254a9b0bdc5
hiten <hitengupta12345@gmail.com>
2025-09-29 16:57:19 +0930

    re

--- week7/FairWorkload/FairWorkload.hpp
+++ week7/FairWorkload/FairWorkload.hpp
@@ -11,7 +11,7 @@ class FairWorkload {
 
             int st = 0, en = sum;
             while (st < en) {
-                int mid = (st + en) / 2;
+                int mid = (st + en + 1) / 2;
 
                 if (isPos(mid, folders, workers)) {
                     en = mid;
@@ -32,10 +32,10 @@ class FairWorkload {
 
                 if (curSum > num) {
                     cab++;
-                    curSum = folders[i - 1];
+                    curSum = folders[i];
                 }
             }
 
-            return cab <= workers;
+            return cab + 1 <= workers;
         }
 };
