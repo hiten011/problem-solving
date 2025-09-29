ea7c405c3ec81fd93e41154020ecb9fd4734f08f
hiten <hitengupta12345@gmail.com>
2025-09-29 16:42:46 +0930

    re

--- week7/FairWorkload/FairWorkload.hpp
+++ week7/FairWorkload/FairWorkload.hpp
@@ -4,23 +4,25 @@ using namespace std;
 class FairWorkload {
     public:
         int getMostWork(vector<int> folders, int workers) {
-            int sum = 0;
-            for (int i : folders) {
-                sum += i;
-            }
+            // int sum = 0;
+            // for (int i : folders) {
+            //     sum += i;
+            // }
 
-            int st = 0, en = sum;
-            while (st < en) {
-                int mid = (st + en + 1) / 2;
+            // int st = 0, en = sum;
+            // while (st < en) {
+            //     int mid = (st + en + 1) / 2;
 
-                if (isPos(mid, folders, workers)) {
-                    en = mid;
-                } else {
-                    st = mid + 1;
-                }
-            }
+            //     if (isPos(mid, folders, workers)) {
+            //         en = mid;
+            //     } else {
+            //         st = mid + 1;
+            //     }
+            // }
 
-            return st;
+            // return st;
+
+            return 0;
         }
 
     private:
