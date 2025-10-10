7368fe488a88e4161d924b5881209cda2b906678
hiten <hitengupta12345@gmail.com>
2025-10-10 21:12:58 +1030

    updated local main

---
+++ week8/RockStart/RockStart.hpp
@@ -0,0 +1,25 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class RockStart
+{
+public:
+    int getNumSongs(int ff, int fs, int sf, int ss) {
+        int ans = 0;
+        if (fs > 0) {
+            ans += ff;
+        } else if (ff != 0) {
+            return ff;
+        }
+
+        if (fs > 0) {
+            ans += fs;
+        } else {
+            return ss + sf;
+        }
+
+        return ans + ss + sf;
+    }
+
+private:
+};
