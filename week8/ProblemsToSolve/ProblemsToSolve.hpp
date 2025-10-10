fe5ee9a529f31c69f011cd9415d1e10da4362e96
hiten <hitengupta12345@gmail.com>
2025-10-10 19:40:31 +1030

    updated local main

--- week8/ProblemsToSolve/ProblemsToSolve.hpp
+++ week8/ProblemsToSolve/ProblemsToSolve.hpp
@@ -5,7 +5,7 @@ class ProblemsToSolve
 {
 public:
     int minNumber(vector<int> pleasantness, int variety) {
-        int minIndex = 0;
+        int minIndex = 0, maxIndex = 0;
         for (int i = 0; i < pleasantness.size(); i++) {
             if (pleasantness[i] < pleasantness[minIndex]) {
                 minIndex = i;
@@ -13,8 +13,14 @@ public:
                 minIndex = i;
             }
 
-            if (pleasantness[i] - pleasantness[minIndex] >= variety) {
-                if (i % 2 == 0 && minIndex % 2 == 0) return (i / 2) + 1;
+            if (pleasantness[i] > pleasantness[maxIndex]) {
+                maxIndex = i;
+            } else if (pleasantness[i] == pleasantness[maxIndex] && i % 2 == 0) {
+                maxIndex = i;
+            }
+
+            if (pleasantness[maxIndex] - pleasantness[minIndex] >= variety) {
+                if (maxIndex % 2 == 0 && minIndex % 2 == 0) return (i / 2) + 1;
                 return (i / 2) + 2;
             }
         }
