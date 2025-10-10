a7c300c2d1192e40b37aa61a49841e08156f9410
hiten <hitengupta12345@gmail.com>
2025-10-10 22:24:26 +1030

    updated local main

---
+++ week8/HandsShaking/HandsShaking.hpp
@@ -0,0 +1,14 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class HandsShaking
+{
+public:
+    long long countPerfect(int n) {
+        if (n % 2 == 1) return 0;
+
+        
+    }
+
+    private:
+};
