dc9278bbe57411922d2e74f6e14af1d17956e2d1
hiten <hitengupta12345@gmail.com>
2025-10-12 00:34:46 +1030

    adding more features

--- main.cpp
+++ main.cpp
@@ -1,6 +1,7 @@
 // personal files
 #include "week9/ErdosNumber/ErdosNumber.hpp"
 #include "week9/SentenceDecomposition/SentenceDecomposition.hpp"
+#include "week9/RoughStrings/RoughStrings.hpp"
 
 int main() {
     ErdosNumber c1;
@@ -8,6 +9,9 @@ int main() {
 
     SentenceDecomposition c2;
     cout << c2.decompose("ommwreehisymkiml", {"we", "were", "here", "my", "is", "mom", "here", "si", "milk", "where", "si"}) << endl;
+
+    RoughString c3;
+    cout << c3.minRoughness("aaaaabbc", 1) << endl;
 
     return 0;
 }
---
+++ week9/RoughStrings/RoughStrings.hpp
@@ -0,0 +1,32 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class RoughString {
+public:
+    int minRoughness(string s, int n) {
+        vector<int> freq(26, 0);
+        for (char c : s) {
+            freq[getInt(c)]++;
+        }
+
+        sort(freq.begin(), freq.end());
+        int st = 0, en = 25, roughness = INT_MAX;
+        while (st < en && n > -1) {
+            if (freq[st] == 0) {
+                st++;
+                continue;
+            }
+            
+            roughness = min(abs(freq[st] - freq[en]), roughness);
+            freq[st]--;
+            n--;
+        }
+
+        return roughness;
+    }
+
+private:
+    int getInt(char c) {
+        return (int) (c - 'a');
+    }
+};
