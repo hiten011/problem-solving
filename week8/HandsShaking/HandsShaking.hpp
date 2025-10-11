fcfce487742b1800857a70389d19934d5abac1a7
hiten <hitengupta12345@gmail.com>
2025-10-11 17:13:51 +1030

    adding more features

--- week8/HandsShaking/HandsShaking.hpp
+++ week8/HandsShaking/HandsShaking.hpp
@@ -7,6 +7,15 @@ public:
     long long countPerfect(int n) {
         if (n % 2 == 1) return 0;
 
+        vector<long long> dp(n + 1, 0);
+        dp[0] = 1;
+        for (int i = 2; i <= n; i += 2) {
+            for (int j = 0; j <= i - 2; j += 2) {
+                dp[i] += dp[j] * dp[i - 2 - j];
+            }
+        }
+
+        return dp[n];
         
     }
 
