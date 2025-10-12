d49d32324e20b1b9cf506791ea3d29ebc42ac73a
hiten <hitengupta12345@gmail.com>
2025-10-12 21:59:25 +1030

    adding more features

--- week9/RoughStrings/RoughStrings.hpp
+++ week9/RoughStrings/RoughStrings.hpp
@@ -9,23 +9,40 @@ public:
             freq[getInt(c)]++;
         }
 
-        sort(freq.begin(), freq.end());
-        int st = 0, en = 25, roughness = INT_MAX;
-        while (st < en && n > -1) {
-            if (freq[st] == 0) {
-                st++;
-                continue;
-            }
-            
-            roughness = min(abs(freq[st] - freq[en]), roughness);
-            freq[st]--;
-            n--;
-        }
-
-        return roughness;
+        return rec(freq, n, 0);
     }
 
 private:
+    int rec(vector<int> &freq, int n, int idx) {
+        if (idx == freq.size()) {
+            return calcRoughness(freq);
+        }
+
+        if (freq[idx] == 0) {
+            return rec(freq, n, idx + 1);
+        }
+
+        int ans = INT_MAX;
+        for (int i = 0; i <= n; i++) {
+            freq[idx] -= i;
+            ans = min(rec(freq, n - i, idx + 1), ans);
+            freq[idx] += i;
+        }
+
+        return ans;
+    }
+
+    int calcRoughness(vector<int> &freq) {
+        int maxValue = INT_MIN, minValue = INT_MAX;
+        for (int i : freq) {
+            if (i == 0) continue;
+            maxValue = max(maxValue, i);
+            minValue = min(minValue, i);
+        }
+
+        return (maxValue == INT_MAX ? INT_MAX : maxValue - minValue);
+    }
+
     int getInt(char c) {
         return (int) (c - 'a');
     }
