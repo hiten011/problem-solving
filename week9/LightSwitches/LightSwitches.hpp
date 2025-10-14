af537b9f65eb583912d5911393670afaca69638a
hiten <hitengupta12345@gmail.com>
2025-10-14 23:25:05 +1030

    adding more features

--- week9/LightSwitches/LightSwitches.hpp
+++ week9/LightSwitches/LightSwitches.hpp
@@ -6,37 +6,44 @@ class LightSwitches {
         long long countPossibleConfigurations(vector<string> switches) {
             // bipartite graphs
             // 1. create adj list
-            vector<vector<int>> adj;
-            int n = switches.size(), k = switches[0].size();
+            n = switches.size(), k = switches[0].size();
+            vector<unordered_set<int>> adjS(n), adjB(k);
             for (int i = 0; i < n; i++) {
                 // switch i
-                vector<int> bulbs;
                 for (int j = 0; j < k; j++) {
                     if (switches[i][j] == 'Y') {
-                        bulbs.push_back(j);
+                        adjS[i].insert(j);
+                        adjB[j].insert(i);
+                        // cout << i << ": " << j << endl;
                     }
                 }
-
-                adj.push_back(bulbs);
             }
-
-            sort(adj.begin(), adj.end(), [](vector<int> &a, vector<int> &b){
-                return a.size() < b.size();
-            });
             
             int ans = 0;
             vector<bool> used(k, false);
             for (int i = 0; i < n; i++) {
-                cout << adj[i].size() << endl;
+                // get vertice with lowest number of edges
+                int node = getSmallest(adjS); //  O(n)
+                // cout << node << ": " << adjS[node].size() << endl;
                 bool isUnique = false;
-                for (int to : adj[i]) {
+                for (int to : adjS[node]) {
                     if (!used[to]) {
-                        isUnique = true;
                         used[to] = true;
+
+                        // erase bulb from adj
+                        for (int s : adjB[to]) {
+                            if (s == node) continue;
+                            adjS[s].erase(to);
+                        }
+
+                        isUnique = true;
                     }
                 }
 
+                adjS[node].clear();
+
                 if (isUnique) ans++;
+                // cout << node << endl;
             }
 
             return pow(2, ans);
@@ -44,5 +51,20 @@ class LightSwitches {
 
     private:
         int n, k;
+        int getSmallest(vector<unordered_set<int>> &adjS) {
+            int minV = -1;
+            for (int i = 0; i < n; i++) {
+                if (adjS[i].size() == 0) continue;
+                if (minV == -1) {
+                    minV = i;
+                }
+
+                if (adjS[minV].size() > adjS[i].size()) {
+                    minV = i;
+                }
+            }
+            
+            return (minV == -1 ? 0 : minV);
+        }
 
 };
