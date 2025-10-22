5d79e86632c4d967bd3f5aee504d104f4873460a
hiten <hitengupta12345@gmail.com>
2025-10-22 12:52:20 +1030

    adding more features

---
+++ week10/BridgeSort/BridgeSort.hpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+class BridgeSort {
+    public:
+        string sortedHand(string hand) {
+            vector<string> str;
+            string temp = "";
+            for (char c : hand) {
+                temp += c;
+
+                if (temp.size() == 2) {
+                    str.push_back(temp);
+                    temp = "";
+                } 
+            }
+
+            sort(str);
+            string ans = "";
+            for (string &s : str) {
+                ans += s;
+            }
+
+            return s;
+        }
+};
